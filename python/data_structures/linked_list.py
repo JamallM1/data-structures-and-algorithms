ea5c4f4e401629cbba7ae2026076c6af62c130a3
Jamall Malik <mimsjamall79@gmail.com>
2022-07-25 22:26:21 -0400

    finished code challenge

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -6,6 +6,7 @@ class LinkedList:
     def __init__(self):
         # initialization here
         self.head = None
+        self.tail = None
 
     def __str__(self):
         text = ""
@@ -13,6 +14,7 @@ class LinkedList:
 
         while current is not None:
             text += "{ " + str(current.value) + " } -> "
+            print(txt)
             current = current.next
         return text + "NULL"
 
@@ -30,10 +32,62 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
+    def append(self, value):
+
+        new_node = Node(value)
+
+        # start at  the beginning or head
+        current = self.head
+
+        while current:
+            # no next
+            if not current.next:
+
+                current.next = new_node
+
+                break
+            else:
+                # moves to the next one
+                current = current.next
+
+    def insert_before(self, search_val, value):
+        new_node = Node(value)
+
+        if self.head.value == search_val:
+            self.insert(value)
+            return
+
+        # start at head or beginning
+        current = self.head
+
+        while current and current.next:
+            # if current node has a value it looks for value
+            if current.next.value == search_val:
+
+                new_node.next = current.next
+
+                current.next = new_node
+
+                break
+            else:
+                current = current.next
+
+    def insert_after(self, search_val, value):
+
+        current = self.head
+
+        while current:
+            if current.value == search_val:
+                self.insert_before(current.next.value, value)
+                return
+            else:
+                current = current.next
+
 
 class Node:
-    def __init__(self, value,):
+    def __init__(self, value, next=None):
         self.value = value
+        self.next = next
 
 
 class TargetError:
