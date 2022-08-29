8516fd68d4c784bc7e73e34f2f56ac06249e8084
Jamall Malik <mimsjamall79@gmail.com>
2022-08-29 18:53:55 -0400

    finished whiteboard and code challenge 31 hash-map repeated word

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,20 @@
 from data_structures.hashtable import Hashtable
+import re
+
+def first_repeated_word(string):
+    regex = re.compile('[^a-zA-Z ]')
+    stripped = regex.sub('', string)
+    words = stripped.lower().split()
+    dict = set()
+
+    for string in words:
+        if string in dict:
+            return string
+        else:
+            dict.add(string)
 
 
-def first_repeated_word():
-    pass
+
+
+
+
---
+++ python/docs/hashtable_repeated_word/README.md
@@ -0,0 +1,12 @@
+
+# Challenge Summary
+- finds the first word to occur more than once in a string and return the string
+
+## Whiteboard Process
+![Code Challenge31](codeChallenge31.png)
+
+## Approach & Efficiency
+- the Big O notation is 0(n)time because it is traversing through a string
+
+
+
