b2a3bedfc7c3df4a5b664b17438ac05423fe5c73
Jamall Malik <mimsjamall79@gmail.com>
2022-09-08 17:22:06 -0400

    finished code challenge

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -22,6 +22,19 @@ class Graph:
                     breadth.enqueue(neighbor.vertex)
         return all_vertices
 
+    def depth_first_search(self, start):
+        def pre_order(vertex, collection=[], visited=set()):
+            if not vertex or vertex not in self._adjacency_list or vertex in visited:
+                return collection
+            collection.append(vertex.value)
+            visited.add(vertex)
+            edge_list = self._adjacency_list[vertex]
+            for edge in edge_list:
+                pre_order(edge.vertex, collection, visited)
+            return collection
+
+        return pre_order(start)
+
     def add_node(self, value):
         vertex = Vertex(value)
         self._adjacency_list[vertex] = []
---
+++ python/docs/graph_depth_first/README.md
@@ -0,0 +1,14 @@
+# Depth First Traversal
+<!-- Short summary or background information -->
+
+## Challenge
+- Conduct a depth first preorder traversal on a graph. Return the collection of nodes in their pre-order depth-first traversal order
+
+## Approach & Efficiency
+- O(N) it is traversing through a graph returning the visited nodes in a list in order it was traversed to.
+
+## Solution
+![Whiteboard](Screen%20Shot%202022-09-08%20at%205.16.03%20PM.png)
+
+## Collaborators
+- Alec, Riki, Ryan
